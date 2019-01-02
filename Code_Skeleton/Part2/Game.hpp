b749feada7d3e99b1204af39cfe849dcbffa4e7f
daniellek12 <danielle.kupitman@gmail.com>
2019-01-02 19:05:09 +0200

    Update Game.hpp

--- Code_Skeleton/Part2/Game.hpp
+++ Code_Skeleton/Part2/Game.hpp
@@ -9,22 +9,22 @@
 #include "PCQueue.hpp"
 #include "utils.hpp"
 
-#define LIVE_CELL 1
-#define DEAD_CELL 0
+#define DEAD_CELL ' '
+#define LIVE_CELL 'X'
 
 struct game_params {
 	// All here are derived from ARGV, the program's input parameters. 
 	uint n_gen;
 	uint n_thread;
 	string filename;
-	bool interactive_on; 
-	bool print_on; 
+	bool interactive_on;
+	bool print_on;
 };
 
 ///----------------hw3----------------///
 struct Task {
-    int row_start;
-    int row_end;
+	int row_start;
+	int row_end;
 };
 ///----------------hw3----------------///
 
@@ -37,7 +37,7 @@ public:
 	Game(game_params);
 	~Game();
 	void run(); // Runs the game
-	const vector<float> gen_hist() const; // Returns the generation timing histogram  
+	const vector<float> gen_hist() const; // Returns the generation timing histogram
 	const vector<float> tile_hist() const; // Returns the tile timing histogram
 	uint thread_num() const; //Returns the effective number of running threads = min(thread_num, field_height)
 
@@ -45,31 +45,31 @@ public:
 protected: // All members here are protected, instead of private for testing purposes
 
 	// See Game.cpp for details on these three functions
-	void _init_game(); 
-	void _step(uint curr_gen); 
-	void _destroy_game(); 
+	void _init_game();
+	void _step(uint curr_gen);
+	void _destroy_game();
 
 	uint m_gen_num; 			 // The number of generations to run
 	uint m_thread_num; 			 // Effective number of threads = min(thread_num, field_height)
-	vector<float> m_tile_hist; 	 // Shared Timing history for tiles: First m_gen_num cells are the calculation durations for tiles in generation 1 and so on. 
-							   	 // Note: In your implementation, all m_thread_num threads must write to this structure. 
+	vector<float> m_tile_hist; 	 // Shared Timing history for tiles: First m_gen_num cells are the calculation durations for tiles in generation 1 and so on.
+	// Note: In your implementation, all m_thread_num threads must write to this structure.
 	vector<float> m_gen_hist;  	 // Timing history for generations: x=m_gen_hist[t] iff generation t was calculated in x microseconds
 	vector<Thread*> m_threadpool; // A storage container for your threads.
 	// This acts as the threadpool.
 
-	bool interactive_on; // Controls interactive mode - that means, prints the board as an animation instead of a simple dump to STDOUT 
+	bool interactive_on; // Controls interactive mode - that means, prints the board as an animation instead of a simple dump to STDOUT
 	bool print_on; // Allows the printing of the board. Turn this off when you are checking performance (Dry 3, last question)
 
 ///----------------hw3----------------///
 
 	PCQueue<Task> queue;
 	bool** current_board;   //field
-    int board_width;
-    int board_height;
+	int board_width;
+	int board_height;
 	bool** next_move_board; //next move field
-    Semaphore barrier;    //Semaphore(0)
-    Semaphore mutex;  //Semaphore(1)
-    int done_tasks_num; //counter for done tasks each generation
+	Semaphore barrier;    //Semaphore(0)
+	Semaphore mutex;  //Semaphore(1)
+	int done_tasks_num; //counter for done tasks each generation
 
 	class Consumer: public Thread{ //a subclass of the thread class, need to
 		// implement thread_workload (to override the implementation of thread)
@@ -80,17 +80,13 @@ protected: // All members here are protected, instead of private for testing pur
 	protected:
 		void thread_workload(){
 			int neighbors_alive = 0;
-			while(true){
-				//where put timer
-
-
-				// WHAT IS TASK
+			while(1){
 				Task* t = game->queue->pop();
 				if(t->row_start == -1){
 					delete(t);
 					return;
 				}
-				//need that t will have some stat time and end time
+				auto start_time = std::chrono::system_clock::now();
 				for(int i = t->row_start; i <= t->row_end; i++){
 					for(uint j = 1; j < game->board_width-1; j++){
 						neighbors_alive = 0;
@@ -113,16 +109,19 @@ protected: // All members here are protected, instead of private for testing pur
 						//a dead cell with exactly 3 neighbors will beacome
 						// alive in the next move
 						if(game->current_board[i][j] == DEAD_CELL &&
-								neighbors_alive == 3){
+						   neighbors_alive == 3){
 							game->next_move_board = LIVE_CELL;
 						}
 					}
 				}
-                game->mutex.down();
-                done_tasks_num++;
-                if(done_tasks_num == m_thread_num)
-                    game->barrier.up();
-                game->mutex.up();
+				game->mutex.down();
+				done_tasks_num++;
+				if(done_tasks_num == m_thread_num)
+					game->barrier.up();
+				auto end_time = std::chrono::system_clock::now();
+				(game->m_tile_hist).push_back((float) std::chrono::duration_cast
+						<std::chrono::microseconds>(end_time - start_time).count());
+				game->mutex.up();
 			}
 		}
 
